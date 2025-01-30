648aa33a17117bb66097aa13c8e2b5373df7b0b5
HAMILTON.AI-DOT <mauro.rivas.7927@gmail.com>
2025-01-30 16:28:14 -0300

    se crea la funcion sortearAmigo(); y se muesta el amigo sorteado

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ function agregarAmigo() {
     const nombreAmigo = inputAmigo.value.trim();
 
     if (nombreAmigo !== "") {
-        // Crear un nuevo elemento de lista para el amigo
+        
         const nuevoAmigo = document.createElement("li");
         nuevoAmigo.textContent = nombreAmigo;
-
-        // Agregar el nuevo amigo a la lista
         document.getElementById("listaAmigos").appendChild(nuevoAmigo);
 
         // Vaciar el input después de añadir el nombre
@@ -16,4 +14,28 @@ function agregarAmigo() {
     } else {
         alert("Por favor, ingresa un nombre válido.");
     }
+}
+
+
+
+
+function sortearAmigo() {
+    // Obtener todos los amigos de la lista
+    const lista = document.getElementById("listaAmigos");
+    const amigos = lista.getElementsByTagName("li");
+
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+
+    
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    /*Obtener el nombre del amigo sorteado*/
+    const amigoSorteado = amigos[indiceAleatorio].textContent;
+
+    /*Mostrar el resultado en la lista de resultados*/
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>Amigo sorteado: ${amigoSorteado}</li>`;
 }
